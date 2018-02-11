ea25609b8fc1e5f52938eafef2434f2150614f37
rild <rild3178ata@gmail.com>
2018-02-11 11:31:04 +0900

    apply viewholder, RecodAdapter

--- app/src/main/java/com/kamiture/kamui/stopwatch/RecordAdapter.java
+++ app/src/main/java/com/kamiture/kamui/stopwatch/RecordAdapter.java
@@ -1,16 +1,13 @@
 package com.kamiture.kamui.stopwatch;
 
 import android.content.Context;
-import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
-import android.icu.text.AlphabeticIndex;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.content.Context;
 import android.widget.TextView;
 
 import java.util.List;
@@ -29,15 +26,21 @@ public class RecordAdapter extends ArrayAdapter<Record>{
     @NonNull
     @Override
     public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // Item の情報を一つにまとめる.
+        ViewHolder viewHolder;
+
         if (convertView == null) {
             convertView = layoutInflater.inflate(R.layout.item, parent, false);
+            viewHolder = new ViewHolder(convertView);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
         }
 
+
         Record item = getItem(position);
-        TextView time =  convertView.findViewById(R.id.item_time);
-        time.setText(String.valueOf(item.elapsedTime));
-        TextView date =  convertView.findViewById(R.id.item_date);
-        date.setText(String.valueOf(item.date));
+        viewHolder.timeTextView.setText(String.valueOf(item.elapsedTime));
+        viewHolder.dateTextView.setText(String.valueOf(item.date));
         return convertView;
     }
 
@@ -46,5 +49,15 @@ public class RecordAdapter extends ArrayAdapter<Record>{
         packageManager = context.getPackageManager();
         layoutInflater = LayoutInflater.from(context);
 
+    }
+
+    class ViewHolder {
+        TextView timeTextView;
+        TextView dateTextView;
+
+        public ViewHolder(View v) {
+            timeTextView =  v.findViewById(R.id.item_time);
+            dateTextView =  v.findViewById(R.id.item_date);
+        }
     }
 }
